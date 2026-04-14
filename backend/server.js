d7acb39e9b7ba95082bf111cef85542c4b1341cf
karthikeyan4747 <karthikeyan53932@gmail.com>
2026-04-14 22:23:45 +0530

    Minor change

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ app.post('/auth/login', async (req,res)=>{
     }
 })
 
-
-app.listen(3000,()=>{
+const PORT = process.env.PORT || 3000
+app.listen(PORT,()=>{
     console.log('http://localhost:3000');
 })
